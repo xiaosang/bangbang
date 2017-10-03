3a885528bd0dc825e08cfd49dc6bc07238df1687
mrzhouxu <794135759@qq.com>
2017-10-03 22:46:01 +0800

    test

--- resources/assets/js/weixin/components/Index.vue
+++ resources/assets/js/weixin/components/Index.vue
@@ -1,22 +1,23 @@
 <template>
-    <div>
-
+    <div class="container">
+        <group>
+            <cell title="title" value="value"></cell>
+        </group>
     </div>
 </template>
 
 <script>
+    import { Group, Cell } from 'vux'
     export default {
         components: {
-
+            Group,
+            Cell
         },
         data(){
             return {}
         },
-        methods:{
-
-        },
         mounted() {
-
+            console.log('Component mounted.')
         }
     }
 </script>

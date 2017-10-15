d8edde017d2f47ad689cef9b68f02ed556c31ad2
于永波 <yuyongbo@marchsoft.cn>
2017-10-15 15:33:44 +0800

    首页统计

--- resources/assets/js/admin/layout/index.vue
+++ resources/assets/js/admin/layout/index.vue
@@ -10,10 +10,14 @@
           <el-col :span="12">
             <el-card class="box-card">
               <div slot="header" class="clearfix">
-                <span style="line-height: 36px;">新增用户</span>
+                <span style="line-height: 36px;">用户
+                  
+                </span>
               </div>
-              <div id="user" style="height: 480px;">
-                
+              <div id="user" style="height: 100px;">
+                <div class="databox-right">
+                  <span class="databox-number azure" >全部：{{ userCount.all_follow }}&nbsp;&nbsp;昨日：{{  userCount.yet_follow }}</span>
+                </div>
               </div>
             </el-card>
           </el-col>
@@ -22,8 +26,8 @@
               <div slot="header" class="clearfix">
                 <span style="line-height: 36px;">测试</span>
               </div>
-               <div id="order" style="height: 480px;">
-                
+               <div id="order" style="height: 100px;">
+
               </div>
             </el-card>
           </el-col>
@@ -60,6 +64,31 @@
     width: 480px;
     /* height: 480px; */
   }
+  .databox.databox-lg .databox-right {
+    -lh-property: 0;
+    width: -webkit-calc(100% - 80px);
+    width: -moz-calc(100% - 80px);
+    width: calc(100% - 80px);
+    height: 80px;
+    padding: 10px 15px;
+    }
+    .databox.databox-lg .databox-number {
+    font-size: 20px;
+    margin: 4px 0 6px;
+}
+.azure {
+    color: #2dc3e8!important;
+    font-size: 50px;
+}
+.databox .databox-number {
+    display: block;
+    font-size: 25px;
+    line-height: 26px;
+    margin: 2px;
+    position: relative;
+    font-family: 'Lucida Sans','trebuchet MS',Arial,Helvetica;
+    font-weight: 700;
+}
 </style>
 
 <script>
@@ -67,7 +96,7 @@
   export default {
     data() {
       return {
-       
+       userCount : [],
       }
     },
     
@@ -123,8 +152,18 @@
 
           
         },
+        setUser: function(){
+          axios.post('index/user').then(response =>{
+            console.log(response.data.result)
+            this.userCount = response.data.result
+
+          }).catch(error => {
+            this.$message("网络错误")
+          })
+        },
         onEcharts: function(){
           this.setTask()
+          this.setUser()
         }
     },
     mounted(){

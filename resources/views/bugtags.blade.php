c6334018608978a119658b3074a275293367bb69
于永波 <yuyongbo@marchsoft.cn>
2017-11-15 22:13:14 +0800

    bugtags

--- resources/views/bugtags.blade.php
+++ resources/views/bugtags.blade.php
@@ -1,7 +1,8 @@
+<meta http-equiv="Access-Control-Allow-Origin" content="*">
 @if (config('app.debug'))
     <script src="https://dn-bts.qbox.me/sdk/bugtags-1.0.3.js"></script>
     <script>
         // VERSION_NAME 替换为项目的版本，VERSION_CODE 替换为项目的子版本
-        new Bugtags('82dfb118f2f76dd2ad2e8b760d2435f6', '0.1.0', '2');
+        new Bugtags('82dfb118f2f76dd2ad2e8b760d2435f6','0.1.0','2');
     </script>
 @endif

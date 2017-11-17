beb53eb001f3411823a680afb3bde46a19b803d3
sangjinchao <603336339@qq.com>
2017-11-17 21:20:19 +0800

    test20

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -80,13 +80,13 @@ class IndexController extends Controller
         $options = $this->options();
         $app = new Application($options);
         $payment = $app->payment;
-        $out_trade_no = date("YmdHis"); //拼一下订单号
+        $out_trade_no = time() . uniqid() ; //拼一下订单号
         $attributes = [
             'trade_type'       => 'JSAPI', // JSAPI，NATIVE，APP...
             'body'             => '助力三月',
             'detail'           => "woshitest",//我这里是通过订单找到商品详情，你也可以自定义
             'out_trade_no'     => $out_trade_no,
-            'total_fee'        => 10000, //因为是以分为单位，所以订单里面的金额乘以100
+            'total_fee'        => 100*100, //因为是以分为单位，所以订单里面的金额乘以100
              'notify_url'       => 'http://bangbang.marchsoft.cn/success', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
              'openid'           => $openId, // trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识，
             // ...

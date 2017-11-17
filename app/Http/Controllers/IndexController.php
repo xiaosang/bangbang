8f5b51a0d25f5f8def210d2841e6fc579b8da472
sangjinchao <603336339@qq.com>
2017-11-17 18:47:38 +0800

    test6

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -101,7 +101,7 @@ class IndexController extends Controller
             // return response()->json(['result'=>$result]);
             $prepayId = $result->prepay_id;
             $config = $payment->configForAppPayment($prepayId);
-            response()->json($config);
+            return response()->json($config);
         }
 
     }
